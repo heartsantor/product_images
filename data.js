477b99cb3c031c99a416b3c6ebc0f428ea4366dc
heartsantor <antorboss.bd@gmail.com>
2020-08-26 16:35:59 +0600

    another 10

--- data.js
+++ data.js
@@ -1,99 +1,199 @@
-const item ={
-    item1: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-%201.jpg?token=AKU7OZVFYDD6KMAT4363BLC7IY3AQ", 
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-%202.jpg?token=AKU7OZTOH2EZ7QNFUKZKA427IY3B2",
-         "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-3.jpg?token=AKU7OZWDADQWBAKAZDUO6ZC7IY3DA",
-          "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-%204.jpg?token=AKU7OZTOYSJGRLP7QINQ5FK7IY3DU"
-        ],
-    item2: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%201.jpg?token=AKU7OZTW4URWE72LW5W5IPC7IY3MM", 
+const item = {
+  item1: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-%201.jpg?token=AKU7OZVFYDD6KMAT4363BLC7IY3AQ",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-%202.jpg?token=AKU7OZTOH2EZ7QNFUKZKA427IY3B2",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-3.jpg?token=AKU7OZWDADQWBAKAZDUO6ZC7IY3DA",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item1/SW0814%20-%204.jpg?token=AKU7OZTOYSJGRLP7QINQ5FK7IY3DU",
+  ],
+  item2: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%201.jpg?token=AKU7OZTW4URWE72LW5W5IPC7IY3MM",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%202.jpg?token=AKU7OZRAADPW4JFI3ITJ7KC7IY3NG",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%203.jpg?token=AKU7OZQXBCTKE3HKNA4ISMK7IY3N2",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%204.jpg?token=AKU7OZXGDLGUANZS3I6M7EC7IY3OO"
-    ],
-    item3: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768%20-%201.jpg?token=AKU7OZXZT5J4SEOILRKC2WK7IY3QA", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%203.jpg?token=AKU7OZQXBCTKE3HKNA4ISMK7IY3N2",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item2/SW3944%20-%204.jpg?token=AKU7OZXGDLGUANZS3I6M7EC7IY3OO",
+  ],
+  item3: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768%20-%201.jpg?token=AKU7OZXZT5J4SEOILRKC2WK7IY3QA",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768%20-%202.jpg?token=AKU7OZTSSPLSSJO52EWKNDK7IY3QY",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768%20-%204.jpg?token=AKU7OZQ6JUOVXRRX6MX6MPS7IY3RO",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768-%203.jpg?token=AKU7OZV753CDMO5GMFKSLV27IY3SG"
-    ],
-    item4: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%201.jpg?token=AKU7OZTYSEI7CARMW6A26RK7IY3VI", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768%20-%204.jpg?token=AKU7OZQ6JUOVXRRX6MX6MPS7IY3RO",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item3/SW5768-%203.jpg?token=AKU7OZV753CDMO5GMFKSLV27IY3SG",
+  ],
+  item4: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%201.jpg?token=AKU7OZTYSEI7CARMW6A26RK7IY3VI",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%202.jpg?token=AKU7OZURR7VKGQZCXZ6QIQS7IY3WA",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%203.jpg?token=AKU7OZSWASXMYFY4UNCIUBK7IY3WU",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%204.jpg?token=AKU7OZWDZHGX3Q3X3W7RDJC7IY3XM"
-    ],
-    item5: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%201.jpg?token=AKU7OZTLBADH53OMTYRQAM27IY3ZM", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%203.jpg?token=AKU7OZSWASXMYFY4UNCIUBK7IY3WU",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item4/SW5768A%20-%204.jpg?token=AKU7OZWDZHGX3Q3X3W7RDJC7IY3XM",
+  ],
+  item5: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%201.jpg?token=AKU7OZTLBADH53OMTYRQAM27IY3ZM",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%202.jpg?token=AKU7OZXYYS6Q63M4WRJHL4S7IY32C",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%203.jpg?token=AKU7OZW54ZHIVE3LCSHTGTC7IY326",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%204.jpg?token=AKU7OZVTTA5RZMSAMS7KLZC7IY33U"
-    ],
-    item6: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%201.jpg?token=AKU7OZQBB44CZIXQRC4KGB27IY37U", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%203.jpg?token=AKU7OZW54ZHIVE3LCSHTGTC7IY326",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item5/SW7219%20-%204.jpg?token=AKU7OZVTTA5RZMSAMS7KLZC7IY33U",
+  ],
+  item6: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%201.jpg?token=AKU7OZQBB44CZIXQRC4KGB27IY37U",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%202.jpg?token=AKU7OZUCTVTX6PDMHJXBLP27IY4AM",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%203.jpg?token=AKU7OZVYUBNKOHG3AR65G6K7IY4BG",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%204.jpg?token=AKU7OZTGAFI3GOKTQSQJJNC7IY4B6"
-    ],
-    item7: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%201.jpg?token=AKU7OZXE7P6OMCTTET3DUB27IY4E6", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%203.jpg?token=AKU7OZVYUBNKOHG3AR65G6K7IY4BG",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item6/SW9968%20-%204.jpg?token=AKU7OZTGAFI3GOKTQSQJJNC7IY4B6",
+  ],
+  item7: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%201.jpg?token=AKU7OZXE7P6OMCTTET3DUB27IY4E6",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%202.jpg?token=AKU7OZWL456YN6HMSPW7GKS7IY4FU",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%203.jpg?token=AKU7OZQY6QAUZO3VTRWJNJC7IY4GI",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%204.jpg?token=AKU7OZURDXQJ5R7WPVZ573C7IY4HA"
-    ],
-    item8: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%201.jpg?token=AKU7OZTS2O44P5VTXDPR44K7IY4IS", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%203.jpg?token=AKU7OZQY6QAUZO3VTRWJNJC7IY4GI",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item7/SW10007%20-%204.jpg?token=AKU7OZURDXQJ5R7WPVZ573C7IY4HA",
+  ],
+  item8: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%201.jpg?token=AKU7OZTS2O44P5VTXDPR44K7IY4IS",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%202.jpg?token=AKU7OZQ4TP4EKI74UIRRITC7IY4JK",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%203.jpg?token=AKU7OZVBOOVBCUYVYKHJ2J27IY4J6",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%204.jpg?token=AKU7OZXJDPCGBJOOJSNJA5C7IY4KU"
-    ],
-    item9: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%201.jpg?token=AKU7OZUX6H67K5LZTWM7O627IY4MC", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%203.jpg?token=AKU7OZVBOOVBCUYVYKHJ2J27IY4J6",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item8/SW10353%20-%204.jpg?token=AKU7OZXJDPCGBJOOJSNJA5C7IY4KU",
+  ],
+  item9: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%201.jpg?token=AKU7OZUX6H67K5LZTWM7O627IY4MC",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%202.jpg?token=AKU7OZU2FPHSYLREMYJEJ627IY4MW",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%203.jpg?token=AKU7OZXEHIL3OFEGLH6JCHC7IY4NQ",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%204.jpg?token=AKU7OZXSSBGWEKNH2GEY2E27IY4OG"
-    ],
-    item10: [
-        "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%201.jpg?token=AKU7OZWI3PAAUIUQY3YMVSC7IY4P2", 
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%203.jpg?token=AKU7OZXEHIL3OFEGLH6JCHC7IY4NQ",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item9/SW10534%20-%204.jpg?token=AKU7OZXSSBGWEKNH2GEY2E27IY4OG",
+  ],
+  item10: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%201.jpg?token=AKU7OZWI3PAAUIUQY3YMVSC7IY4P2",
     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%202.jpg?token=AKU7OZRH5LKB7J4CVABRBBK7IY4Q6",
-     "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%203.jpg?token=AKU7OZWLAZLCQTWGBWZG4DS7IY4RY",
-      "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%204.jpg?token=AKU7OZRUDY4EPY3RGJNNBMC7IY4SK"
-    ],
-    item11: [
-        "link", 
-    "link2",
-     "link3",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%203.jpg?token=AKU7OZWLAZLCQTWGBWZG4DS7IY4RY",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item10/SW10954%20-%204.jpg?token=AKU7OZRUDY4EPY3RGJNNBMC7IY4SK",
+  ],
+  item11: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item11/SW10975%20-%201.jpg?token=AKU7OZW5I5QAX4R6XJNUF2S7IY424",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item11/SW10975%20-%202.jpg?token=AKU7OZQD2ECCOZM2JB36MFS7IY43U",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item11/SW10975%20-%203.jpg?token=AKU7OZQOHRKI422ARQXYOPS7IY44U",
+  ],
+  item12: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item12/SW11176%20-%201.jpg?token=AKU7OZQDAT6ZXAZCL3ISARS7IY464",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item12/SW11176%20-%202.jpg?token=AKU7OZRF2DMJHIIRAPMSVXK7IY47S",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item12/SW11176%20-%203.jpg?token=AKU7OZTGHELKCL7OIKR2H327IY5AI",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item12/SW11176%20-%204.jpg?token=AKU7OZUD7XXM5CCV6YZ73Z27IY5A4",
+  ],
+  item13: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item13/SW11176A%20-%201.jpg?token=AKU7OZV4PEGYDRDZALMWVLC7IY5CI",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item13/SW11176A%20-%202.jpg?token=AKU7OZRLSV54OAXYBGKTKOC7IY5C4",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item13/SW11176A%20-%203.jpg?token=AKU7OZUIHQMY4XDEDPS4RW27IY5DO",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item13/SW11176A%20-%204.jpg?token=AKU7OZVDLSOHMRWHQC6D6EK7IY5EC",
+  ],
+  item14: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item14/SW11501%20-%201.jpg?token=AKU7OZS46GSGQG3XHLG4VGC7IY5F4",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item14/SW11501%20-%202.jpg?token=AKU7OZUHYBMMGVB3YUNMVWC7IY5GS",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item14/SW11501%20-%203.jpg?token=AKU7OZQNABXK6BTYNNXD3627IY5HE",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item14/SW11501%20-%204.jpg?token=AKU7OZSYQZST7A5F7RDKPBK7IY5HW",
+  ],
+  item15: [
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item15/SW11539%20-%201.jpg?token=AKU7OZRT4AHBCAAZMVLCBFC7IY5I6",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item15/SW11539%20-%202.jpg?token=AKU7OZSOZQHDDAF37O2S3N27IY5JW",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item15/SW11539%20-%203.jpg?token=AKU7OZU5J76TDWZAJQTPVMS7IY5KI",
+    "https://raw.githubusercontent.com/heartsantor/product_images/master/fashion_women/item15/SW11539%20-%204.jpg?token=AKU7OZTCMPYDBCTMDJHQMRS7IY5KY",
+  ],
+  item16: [
+      "link", 
+      "link2", 
+      "link3", 
       "link4"
     ],
-    item12: [
+    item17: [
         "link", 
-    "link2",
-     "link3",
-      "link4"
-    ],
-    item13: [
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item18: [
         "link", 
-    "link2",
-     "link3",
-      "link4"
-    ],
-    item14: [
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item19: [
         "link", 
-    "link2",
-     "link3",
-      "link4"
-    ],
-    item15: [
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item20: [
         "link", 
-    "link2",
-     "link3",
-      "link4"
-    ],
-    item16: [
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item21: [
         "link", 
-    "link2",
-     "link3",
-      "link4"
-    ],
-
-}
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item22: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item23: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item24: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item25: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item26: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item27: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item28: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item29: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item30: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item31: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item32: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+      item33: [
+        "link", 
+        "link2", 
+        "link3", 
+        "link4"
+      ],
+};
